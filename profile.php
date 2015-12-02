18435c863c7ac76c310a64e50335b81f187c9898
sergiomieic <sergio.mieic@gmail.com>
2015-12-02 00:11:26 +0000

    profile basis init

---
+++ profile.php
@@ -0,0 +1,40 @@
+<?php 
+	require 'header.php';
+?>
+
+<?php
+/*
+	if(!isset($_GET["id"]) ){
+			echo "404";
+	} else{
+	 $result = getUser($_GET["id"]); ?>	
+	}
+	*/
+		 
+?>
+
+
+<!-- <img src="imagem" alt="imagemuser" style="width:256px;height:148px;"> -->
+
+
+<!-- verificar atributos -->
+
+<h1> 
+	<?php /* echo $result['name']*/ ?>
+</h1>
+
+<h3> 
+	<?php /*echo $result['data nascimento']*/  ?>	
+</h3>
+<h3> 
+	<?php /*echo $result['descricao']*/	  ?> 	
+</h3>
+
+<!-- ..... -->
+
+
+
+
+<?php 
+	require 'footer.php';
+?>
